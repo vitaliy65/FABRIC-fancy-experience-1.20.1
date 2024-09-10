e69bc6fa976f0f99a21dd52e77e7ce74857502a3
vitaliy65 <vitaliy60606@gmail.com>
2024-09-10 22:49:51 +0300

    mod-created

---
+++ src/main/java/com/vurbin/fancyexperience/FancyExperienceDataGenerator.java
@@ -0,0 +1,11 @@
+package com.vurbin.fancyexperience;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class FancyExperienceDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
